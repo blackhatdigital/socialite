154573caccb7fb294a6d4bf5ea06117682649f9c
Josh <josh@blackhatdigital.com.au>
2016-04-04 12:01:03 +1000

    Add Validations to Form Fields

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,8 @@ Rails.application.configure do
 
   #Needed for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  #Needed for paperclip to work on Windows 7 +
+  Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
