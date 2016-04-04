cb7e202a3dfbdff36e734c6bfa66a9ccdc85ef78
Josh <josh@blackhatdigital.com.au>
2016-04-04 10:57:15 +1000

    Added Static Pages Terms About and Privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/terms'
+
+  get 'pages/privacy'
+
   devise_for :users
   resources :boosts
   # The priority is based upon order of creation: first created -> highest priority.
